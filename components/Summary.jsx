25cf44a3979093aed90ec33eda697bc21edded0e
Samuel Reed <samuelreed01@gmail.com>
2022-02-25 15:35:00 +0700

    add save animation and confirmation

--- components/Summary.jsx
+++ components/Summary.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RoomQuantity from "./RoomQuantity";
 import OptionsList from "./OptionsList";
 import Sidebar from "./Sidebar";
 
 export default function Summary(props) {
+    const [buttonText, setButtonText] = useState("Save Project");
+
     function checkOptions() {
         if (props.options.ipad == "Ya" || props.options.battery == "Ya" || props.options.starter == "Ya") {
             return true
@@ -11,6 +13,34 @@ export default function Summary(props) {
             return false
         }
     };
+
+    async function saveProject() {
+        //display animation
+        const newButtonText =
+            <span>
+                Saving...
+                <div class="spinner-border mx-2" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </span>
+        setButtonText(newButtonText)
+
+        const url = "http://localhost:3000/api/savetodbAPI"
+        const request = new XMLHttpRequest();
+        request.open("POST", url, true);
+        request.onreadystatechange = ()=> {
+            if (request.readyState == 4 && request.status == 200) {
+                console.log("server response: " + request.response);
+                setButtonText("Saved")
+              }
+        }
+        request.send(JSON.stringify({id: props.projectId, projectName: props.projectName, roomList: props.roomList, options: props.options}));
+    }
+
+    useEffect( ()=> {
+        setButtonText("Save Project")
+    }, [props.roomList])
+
     return (
         <div className="container row m-0 p-0 h-100 w-100">
             <div className="col-8 h-100 px-6 overflow-auto">
@@ -41,7 +71,7 @@ export default function Summary(props) {
                     <button className="button btn-dark mx-3" onClick={() => props.setAppState("moreoptions")}>More Options</button>
                     <br/>
                     <br/>
-                    <button className="button btn-dark mx-3" onClick={props.saveProject}>Save Project</button>
+                    <button className="button btn-dark mx-3" onClick={saveProject}>{buttonText}</button>
                 </div>
                 
             </div>
