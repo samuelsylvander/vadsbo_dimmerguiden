b0ab66ce2a2881b194fdbf786195f3e57b4fb9ec
Samuel Reed <samuelreed01@gmail.com>
2022-02-28 10:51:44 +0700

    change header to copy link on click

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,29 @@
 import React, {useState} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
-import Popup from "./Popup";
 
 function Header(props) {
     const url = `localhost:3000/${props.projectId}`;
-    const [showPopup, setShowPopup] = useState(false)
     const body = <>Your project ID is: {props.projectId}<br />Access this project at:<br/>{url}</>
 
     function copyURL() {
         navigator.clipboard.writeText(url);
+        const popup = document.getElementById("project-url");
+        popup.style.display = "block";
+        setTimeout(()=>popup.style.display = "none", 2000);
     }
+
 
     return (
         <nav className="navbar bg-primary position-fixed w-100">
-            <span className="navbar-brand">Vadsbo</span>
-            {props.projectId && <FontAwesomeIcon icon={faLink} onClick={()=>setShowPopup(true)} />}
-            {showPopup && <Popup
-                title="Share your project"
-                body={body}
-                confirm={["Copy to Clipboard", copyURL]}
-                dismiss={["Close", ()=>setShowPopup(false)]}
-            />}
-        </nav>
+                <div className="col-auto px-3 me-auto">
+                    <span className="navbar-brand">Vadsbo</span>
+                </div>
+                {props.projectId && <div className="col-1 text-center" onClick={copyURL}>
+                    <FontAwesomeIcon className="mx-3" icon={faLink} />
+                    <div id="project-url" style={{display: "none"}} className="bg-info position-absolute">URL Copied to Clipboard</div>  
+                </div>}
+         </nav>
     )
 }
 
