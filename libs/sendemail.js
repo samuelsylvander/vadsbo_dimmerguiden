cc9b38e9bacba95e5044c7d7d429b457c71dbaa9
Samuel Reed <samuelreed01@gmail.com>
2022-03-07 17:23:17 +0700

    update sendemail to use fetch api

--- libs/sendemail.js
+++ libs/sendemail.js
@@ -3,20 +3,19 @@ export default async function sendEmail(input) {
     for (var pair of input.entries()) {
         email[pair[0]] = pair[1] 
     }
-    console.log(email)
     const url = "../api/sendemailAPI"
-    const request = new XMLHttpRequest();
-    try {
-        request.open("POST", url, false);
-        request.onreadystatechange = ()=> {
-            if (request.readyState == 4 && request.status == 200) {
-                console.log("server response: " + request.response)
-            }
-        }
-        request.send(JSON.stringify(email));
-        return "success"
-    } catch (error) {
-        console.log("error in sendemail.js: " + error)
-        return "error"
-    }
+    const request = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(email)
+    })
+        .then(response => response.json())
+        .then(response => {
+            console.log("server response: " + JSON.stringify(response))
+            return "success"
+        })
+        .catch(error => {
+            console.log("error in sendemail.js: " + error)
+            return "error"
+        })
+    return request
 }
