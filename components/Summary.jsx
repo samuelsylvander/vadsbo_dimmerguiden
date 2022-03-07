93962841941f1cec4f74a6faf2c86f09a5673eb0
Samuel Reed <samuelreed01@gmail.com>
2022-03-07 11:52:16 +0700

    add test modal

--- components/Summary.jsx
+++ components/Summary.jsx
@@ -17,6 +17,10 @@ export default function Summary(props) {
         }
     };
 
+    function handleDelete() {
+
+    }
+
     async function saveProject() {
         toast.current.show();
         const url = "http://localhost:3000/api/savetodbAPI"
@@ -51,7 +55,7 @@ export default function Summary(props) {
                             roomList={props.roomList} 
                             setRoomList={props.setRoomList} 
                             loadRoom={props.loadRoom} 
-                            deleteRoom={props.deleteRoom} 
+                            deleteRoom={handleDelete} 
                         />
                     )
                 })}
@@ -67,6 +71,7 @@ export default function Summary(props) {
                     <button className="btn btn-dark mx-3" onClick={props.addRoom}>Lägg till rum</button>
                     <button className="btn btn-dark mx-3" onClick={() => props.setAppState("moreoptions")}>Tillval</button>
                     <button className="btn btn-dark mx-3" onClick={()=>setShowPopup(true)}>Dela</button>
+                    <button className="btn btn-dark mx-3" data-bs-toggle="modal" data-bs-target="#confirmDelete">Modal</button>
                 </div>
                 
             </div>
@@ -86,6 +91,25 @@ export default function Summary(props) {
                             Project Saved!
                         </div>
                         <button type="button" className="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="modal fade" id="confirmDelete" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="confirmDeleteLabel">Are you sure you want to delete?</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            This action cannot be undone.
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" className="btn btn-primary"onClick={props.deleteRoom}>Delete</button>
+                        </div>
                     </div>
                 </div>
             </div>
