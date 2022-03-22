6ace2836e49cdaee0d95d65ec0220cba22e249c8
Samuel Reed <samuelreed01@gmail.com>
2022-03-22 13:08:01 +0700

    reinstate SwitchButtons changes

--- components/SwitchButtons.jsx
+++ components/SwitchButtons.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Info from "./Info";
-import daliLogo from "../public/dali_r_logo_black.png";
-import Image from "next/image";
+
 
 //props required
 // label
 // infoText (currently unused)
 // property - what value will actually be set
 // field - array containing button labels, also values to be set to property
+// (images - array containing images to display. Not required, but will replace field on button display)
 // currentRoom - object with current room details
 // setCurrentRoom - function to set room details
 
@@ -17,25 +17,12 @@ function SwitchButtons(props) {
         props.setCurrentRoom(prevVal => ({...prevVal, [props.property]: buttonField}))
     }
 
-    function formatField(field) {
-        if (field.includes("DALI")) {
-            return (
-            <div className="d-flex align-items-center fw-bold">
-                <Image src={daliLogo} height={35} width={110} alt="DALI"/>
-                {field.slice(4)}
-            </div>
-            )
-        } else {
-            return field
-        }
-    }
-
     return (
         <div className="container">
             <h3 className="d-inline-block my-4">{props.label}</h3>
             {props.infoText && <Info text={props.infoText} />}
             <div>
-                {props.field.map((field) => {
+                {props.field.map((field, index) => {
                     return (
                         <button 
                             key={field}
@@ -44,7 +31,7 @@ function SwitchButtons(props) {
                             className={props.currentRoom[props.property] == field ? "btn btn-primary mx-2 p-2 px-3" : "btn btn-outline-dark mx-2 p-2 px-3"} 
                             onClick={handleSwitch}
                         >
-                            {formatField(field)}
+                            {props.images ? props.images[index] : field}
                         </button>
                     )
                 })}
