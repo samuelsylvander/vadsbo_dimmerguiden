48c06ebd577563d7558f4ede3d0fcaa9188d08c1
Samuel Reed <samuelreed01@gmail.com>
2022-07-15 11:29:18 +0700

    fix _id bug in savetodbAPI

--- pages/api/savetodbAPI.js
+++ pages/api/savetodbAPI.js
@@ -3,20 +3,21 @@ import { ObjectId } from "mongodb";
 
 export default async function updateDB(req, res) {
 	const requestObject = JSON.parse(req.body);
+	delete requestObject._id;
 
 	try {
 		const { db } = await connectToDatabase();
 		const vadsbo = await db.collection("projects");
 
-		if (!requestObject.id) {
+		if (!requestObject.project_id) {
 			const result = await vadsbo.insertOne(requestObject);
 			// console.log(result);
 			res.status(200).json(result);
 			res.send();
-			await vadsbo.updateOne({ _id: result.insertedId }, { $set: { id: result.insertedId } });
+			await vadsbo.updateOne({ _id: result.insertedId }, { $set: { project_id: result.insertedId } });
 			console.log("updated id to: " + result.insertedId);
 		} else {
-			const filter = { _id: new ObjectId(requestObject.id) };
+			const filter = { _id: new ObjectId(requestObject.project_id) };
 			// const update = {$set: {roomList: requestObject.roomList}};
 			const result = await vadsbo.replaceOne(filter, requestObject);
 			console.log(result);
